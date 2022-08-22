ddc8f2c2459e9f3335882c2d67c7ec02b13e4d4b
sneha jacob <snehajacobparavila.gmail.com>
2022-08-22 15:54:21 +0530

    Removed Files

--- LastProject/library/src/main/java/com/example/library/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.library;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<UserModel, String> {
-
- 
-
-}
--- apis/src/main/java/com/javatechie/spring/mongo/apis/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javatechie.spring.mongo.apis;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookRepository extends MongoRepository <Book, String>{
-
-}
